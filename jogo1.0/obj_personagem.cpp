6aa36687c80ebaa72be9fb4cc3bdbeade4571f18
jg-moren <67434311+jg-moren@users.noreply.github.com>
2020-11-19 11:59:17 -0300

    Add files via upload

---
+++ jogo1.0/obj_personagem.cpp
@@ -0,0 +1,27 @@
+#include <GL/glut.h>
+#include <stdlib.h>
+
+
+class obj_personagem{
+
+	public:
+		
+		void personagem(float coo[3]){	
+
+		
+		glPushMatrix();	
+		
+		glTranslatef(coo[0],coo[1],coo[2]);
+			glColor3f(1,0,0);
+			glutSolidSphere(1.0,100,100);		
+		glPopMatrix();
+
+		
+
+
+}
+
+		
+	
+};
+
