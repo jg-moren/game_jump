bc52dfe8ef719df075b3006fad05ef74492e4ec4
jg-moren <67434311+jg-moren@users.noreply.github.com>
2020-11-27 19:25:24 -0300

    Add files via upload

---
+++ game/LoadBMP.h
@@ -0,0 +1,22 @@
+#ifndef  FILE_H
+#define  FILE_H
+
+#include <windows.h>
+#include <GL/gl.h>
+#include <GL/glut.h>
+#include <stdio.h>
+class LoadBMP
+{
+  public:
+    loadBMP(char *filename);
+};
+
+#endif  /*FILE_H*/
+
+
+
+
+
+
+
+
